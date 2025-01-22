d0c11d5e89fbcaad7eb683db8757e1c414519594
Amrillo <amrishka88@gmail.com>
2025-01-22 13:20:26 +0500

    fix: adjust path to the text file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 
-const readStr = fs.createReadStream('text.txt', 'utf-8');
+
+const readStr = fs.createReadStream(path.join(__dirname, "text.txt"), 'utf-8');
 let str = '';
 
 readStr.on('data', (chunk) => {
